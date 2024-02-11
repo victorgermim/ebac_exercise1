3781ac9c4b5212a78b9f6d4c7a139e438c6ebe4d
Victor Germim Prado <152669348+victorgermim@users.noreply.github.com>
2024-02-11 19:36:51 -0300

    First commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Função para multiplicar dois números
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+// Função para retornar uma saudação
+function greet(name) {
+    return `Olá ${name}`;
+}
+const product = multiply(4, 5); // Resultado = 20
+console.log(product);
+const greeting = greet("Mundo"); // Resultado = "Olá Mundo"
+console.log(greeting);
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+// Função para multiplicar dois números
+function multiply(num1: number, num2: number): number {
+    return num1 * num2;
+  }
+  
+  // Função para retornar uma saudação
+  function greet(name: string): string {
+    return `Olá ${name}`;
+  }
+  
+
+  const product = multiply(4, 5); // Resultado = 20
+  console.log(product);
+  
+  const greeting = greet("Mundo"); // Resultado = "Olá Mundo"
+  console.log(greeting);
+  
